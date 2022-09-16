2fcf7f94ad777c97d4fd02b182937b87a95cdadc
infastin <thecookied123@gmail.com>
2022-09-16 20:30:22 +0500

    something

--- stack/main.cpp
+++ stack/main.cpp
@@ -9,13 +9,14 @@
 
 void test_temp()
 {
-	std::cout << "Temporary\n";
+	std::cout << "---- Test Fill ----" << '\n';
+	std::cout << "--- Temporary ---\n";
 
 	stack::array<std::string> test_array;
 	stack::list<std::string> test_list;
 	std::stack<std::string> test_std;
 
-	std::cout << "Stack Array" << '\n';
+	std::cout << "-- Stack Array --" << '\n';
 
 	clock_t oldc = clock();
 	stack::fill_random_temp(test_array, 100000);
@@ -24,7 +25,7 @@ void test_temp()
 
 	std::cout << "Processor time: " << diff << " s.\n";
 
-	std::cout << "Stack List" << '\n';
+	std::cout << "-- Stack List --" << '\n';
 
 	oldc = clock();
 	stack::fill_random_temp(test_list, 100000);
@@ -33,7 +34,7 @@ void test_temp()
 
 	std::cout << "Processor time: " << diff << " s.\n";
 
-	std::cout << "Stack Std" << '\n';
+	std::cout << "-- Stack Std --" << '\n';
 
 	oldc = clock();
 	stack::fill_random_temp(test_std, 100000);
@@ -45,52 +46,169 @@ void test_temp()
 
 void test_const()
 {
-	std::cout << "Constant\n";
+	std::cout << "--- Constant ---\n";
 
 	stack::array<std::string> test_array;
 	stack::list<std::string> test_list;
 	std::stack<std::string> test_std;
 
-	std::cout << "Stack Array" << '\n';
+	std::cout << "-- Stack Array --" << '\n';
+
+	const std::string alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+	std::string *random_array = new std::string[100000];
+	for (size_t i = 0; i < 100000; ++i) {
+		random_array[i] = random_string(alphabet, 1000);
+	}
 
 	clock_t oldc = clock();
-	stack::fill_random_const(test_array, 100000);
+	stack::fill_random_const(test_array, 100000, random_array);
 	clock_t newc = clock();
 	double diff = (double) (newc - oldc) / (double) CLOCKS_PER_SEC;
 
 	std::cout << "Processor time: " << diff << " s.\n";
 
-	std::cout << "Stack List" << '\n';
+	std::cout << "-- Stack List --" << '\n';
+
+	for (size_t i = 0; i < 100000; ++i) {
+		random_array[i] = random_string(alphabet, 1000);
+	}
 
 	oldc = clock();
-	stack::fill_random_const(test_list, 100000);
+	stack::fill_random_const(test_array, 100000, random_array);
 	newc = clock();
 	diff = (double) (newc - oldc) / (double) CLOCKS_PER_SEC;
 
 	std::cout << "Processor time: " << diff << " s.\n";
 
-	std::cout << "Stack Std" << '\n';
+	std::cout << "-- Stack Std --" << '\n';
+
+	for (size_t i = 0; i < 100000; ++i) {
+		random_array[i] = random_string(alphabet, 1000);
+	}
 
 	oldc = clock();
-	stack::fill_random_const(test_std, 100000);
+	stack::fill_random_const(test_array, 100000, random_array);
 	newc = clock();
 	diff = (double) (newc - oldc) / (double) CLOCKS_PER_SEC;
 
 	std::cout << "Processor time: " << diff << " s.\n";
+
+	delete[] random_array;
+}
+
+void test_remove()
+{
+	std::cout << "---- Remove All ----\n";
+
+	stack::array<int> test_array;
+
+	test_array.push(10);
+	test_array.push(10);
+	test_array.push(20);
+	test_array.push(10);
+	test_array.push(31);
+	test_array.push(14);
+	test_array.push(10);
+	test_array.push(33);
+	test_array.push(10);
+
+	stack::remove_all(test_array, 10);
+
+	while (!test_array.empty()) {
+		std::cout << test_array.top() << ' ';
+		test_array.pop();
+	}
+
+	std::cout << '\n';
+}
+
+void test_equal()
+{
+	std::cout << "---- Equal ----\n";
+
+	stack::array<int> haystack;
+
+	haystack.push(10);
+	haystack.push(10);
+	haystack.push(20);
+	haystack.push(10);
+	haystack.push(31);
+	haystack.push(14);
+	haystack.push(10);
+	haystack.push(33);
+	haystack.push(10);
+
+	stack::list<int> needle;
+
+	needle.push(10);
+	needle.push(10);
+	needle.push(20);
+	needle.push(10);
+	needle.push(31);
+	needle.push(14);
+	needle.push(10);
+	needle.push(33);
+	needle.push(10);
+
+	std::cout << stack::equal<stack::array<int>, stack::list<int>, int>(haystack, needle) << '\n';
+}
+
+void test_substack()
+{
+	std::cout << "---- Substack ----\n";
+
+	stack::array<int> haystack;
+
+	haystack.push(10);
+	haystack.push(10);
+	haystack.push(20);
+	haystack.push(10);
+	haystack.push(31);
+	haystack.push(14);
+	haystack.push(10);
+	haystack.push(33);
+	haystack.push(10);
+
+	stack::list<int> needle;
+
+	needle.push(20);
+	needle.push(10);
+	needle.push(31);
+
+	std::cout << stack::is_substack<stack::array<int>, stack::list<int>, int>(haystack, needle) << '\n';
+}
+
+void test_sort()
+{
+	std::cout << "---- Sort ----\n";
+
+	stack::array<int> test_array;
+
+	test_array.push(1);
+	test_array.push(4);
+	test_array.push(0);
+	test_array.push(-1);
+	test_array.push(30);
+
+	stack::sort(test_array);
+
+	while (!test_array.empty()) {
+		std::cout << test_array.top() << ' ';
+		test_array.pop();
+	}
+
+	std::cout << '\n';
 }
 
 int main(int argc, char *argv[])
 {
-	stack::array<int> sa;
-	std::stack<int> sstd;
-
-	sa.push(11);
-	sa.push(12);
-	sa.push(14);
-
-	sstd.push(15);
-
-	std::cout << stack::is_substack<stack::array<int>, std::stack<int>, int>(sa, sstd) << std::endl;
+	test_temp();
+	test_const();
+	test_remove();
+	test_substack();
+	test_sort();
+	test_equal();
 
 	return 0;
 }
--- stack/src/stack_utils.hpp
+++ stack/src/stack_utils.hpp
@@ -10,8 +10,8 @@
 
 namespace stack {
 
-template<typename T>
-void fill_random_const(T &s, size_t len)
+template<typename Stack>
+void fill_random_temp(Stack &s, size_t len)
 {
 	const std::string alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
@@ -21,25 +21,25 @@ void fill_random_const(T &s, size_t len)
 	}
 }
 
-template<typename T>
-void fill_random_temp(T &s, size_t len)
+template<typename Stack>
+void fill_random_const(Stack &s, size_t len, std::string *random_array)
 {
 	const std::string alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
 	for (size_t i = 0; i < len; ++i) {
-		s.push(random_string(alphabet, 1000));
+		s.push(std::move(random_array[i]));
 	}
 }
 
-template<typename T, typename S, typename A>
-bool equal(const T &s1, const S &s2)
+template<typename Stack1, typename Stack2, typename T>
+bool equal(const Stack1 &s1, const Stack2 &s2)
 {
-	T copy_s1 = s1;
-	S copy_s2 = s2;
+	Stack1 copy_s1 = s1;
+	Stack2 copy_s2 = s2;
 
 	while (!copy_s1.empty()) {
-		const A &v1 = copy_s1.top();
-		const A &v2 = copy_s2.top();
+		const T &v1 = copy_s1.top();
+		const T &v2 = copy_s2.top();
 
 		if (v1 != v2)
 			return false;
@@ -54,27 +54,28 @@ bool equal(const T &s1, const S &s2)
 	return true;
 }
 
-template<typename T, typename A>
-void remove_all(T &s, const A &value)
+template<typename Stack, typename T>
+void remove_all(Stack &s, const T &value)
 {
-	std::vector<A> vec;
+	Stack temp;
 
 	while (!s.empty()) {
-		A &val = s.top();
+		T &val = s.top();
 
 		if (val != value)
-			vec.push_back(std::move(val));
+			temp.push(std::move(val));
 
 		s.pop();
 	}
 
-	for (auto iter = vec.rbegin(); iter != vec.rend(); ++iter) {
-		s.push(std::move(*iter));
+	while (!temp.empty()) {
+		s.push(std::move(temp.top()));
+		temp.pop();
 	}
 }
 
-template<typename T, typename S, typename A>
-bool is_substack(const T &haystack, const S &needle)
+template<typename Stack1, typename Stack2, typename T>
+bool is_substack(const Stack1 &haystack, const Stack2 &needle)
 {
 	if (needle.size() == 0)
 		return true;
@@ -82,11 +83,11 @@ bool is_substack(const T &haystack, const S &needle)
 	if (haystack.size() == 0)
 		return false;
 
-	T copy_haystack = haystack;
-	S copy_needle = needle;
+	Stack1 copy_haystack = haystack;
+	Stack2 copy_needle = needle;
 
-	A *harr = new A[haystack.size()];
-	A *narr = new A[needle.size()];
+	T *harr = new T[haystack.size()];
+	T *narr = new T[needle.size()];
 
 	for (size_t i = haystack.size() - 1;; --i) {
 		harr[i] = std::move(copy_haystack.top());
@@ -104,12 +105,41 @@ bool is_substack(const T &haystack, const S &needle)
 			break;
 	}
 
-	bool result = memmem(harr, haystack.size() * sizeof(A), narr, needle.size() * sizeof(A)) != NULL;
+	bool result = memmem(harr, haystack.size() * sizeof(T), narr, needle.size() * sizeof(T)) != NULL;
 
 	delete[] harr;
 	delete[] narr;
 
 	return result;
+}
+
+template<typename Stack>
+void sort(Stack &s)
+{
+	Stack temp;
+
+	while (!s.empty()) {
+		temp.push(std::move(s.top()));
+		s.pop();
+	}
+
+	while (!temp.empty()) {
+		auto value = std::move(temp.top());
+		temp.pop();
+
+		while (1) {
+			if (s.empty())
+				break;
+
+			if (s.top() <= value)
+				break;
+
+			temp.push(std::move(s.top()));
+			s.pop();
+		}
+
+		s.push(std::move(value));
+	}
 }
 
 }  // namespace stack
