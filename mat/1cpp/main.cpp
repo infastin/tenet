c637cce88cef1065f8521714f9d9b677ae6ba8db
infastin <thecookied123@gmail.com>
2022-11-02 16:11:53 +0500

    finish mat 1

--- mat/1cpp/main.cpp
+++ mat/1cpp/main.cpp
@@ -1,22 +1,69 @@
 #include "path.hpp"
 
 #include <fmt/core.h>
+#include <fmt/ostream.h>
+#include <fstream>
 #include <iostream>
+
+SquareMatrix<double> read_matrix(std::istream &stream)
+{
+	size_t n = 0;
+	stream >> n;
+
+	SquareMatrix<double> result(n);
+
+	for (size_t i = 0; i < n; ++i) {
+		for (size_t j = 0; j < n; ++j) {
+			double number;
+			stream >> number;
+			result[i, j] = number;
+		}
+	}
+
+	return result;
+}
 
 int main(int argc, char *argv[])
 {
-	AdjacencyMatrix<double> matr{
-		{ 0, 0, 0, 14, 15, 0, 0, 0, 0, 8 },
-		{ 0, 0, 0, 0, 0, 0, 0, 0, 20, 0 },
-		{ 0, 0, 0, 0, 0, 0, 14, 0, 19, 0 },
-		{ 14, 0, 0, 0, 6, 0, 0, 0, 0, 11 },
-		{ 15, 0, 0, 6, 0, 0, 0, 0, 0, 0 },
-		{ 0, 0, 0, 0, 0, 0, 19, 4, 18, 0 },
-		{ 0, 0, 14, 0, 0, 19, 0, 15, 0, 0 },
-		{ 0, 0, 0, 0, 0, 4, 15, 0, 0, 0 },
-		{ 0, 20, 19, 0, 0, 18, 0, 0, 0, 0 },
-		{ 8, 0, 0, 11, 0, 0, 0, 0, 0, 0 },
-	};
+	if (argc != 3) {
+		fmt::print(std::cerr, "Expected two arguments\n");
+		exit(EXIT_FAILURE);
+	}
+
+	std::ifstream file;
+	file.open(argv[1]);
+
+	size_t start;
+	sscanf(argv[2], "%lu", &start);
+
+	SquareMatrix<double> matr = read_matrix(file);
+	std::vector<double> dist_dijkstra = dijkstra(matr, start);
+
+	fmt::print("Dijkstra:\n");
+	for (auto d : dist_dijkstra) {
+		fmt::print("{: <6.2f} ", d);
+	}
+
+	auto [dist_ford, has_negative] = ford_bellman(matr, start);
+
+	fmt::print("\nFord Bellman:\n");
+	if (has_negative) {
+		fmt::print("Negative");
+	} else {
+		for (auto d : dist_ford) {
+			fmt::print("{: <6.2f} ", d);
+		}
+	}
+
+	SquareMatrix<double> matr_floyd = floyd_warshall(matr);
+
+	fmt::print("\nFloyd Warshall:\n");
+	for (size_t i = 0; i < matr.size(); ++i) {
+		for (size_t j = 0; j < matr.size(); ++j) {
+			fmt::print("{: <8.2f}", matr[i, j]);
+		}
+		fmt::print("\n");
+	}
 
 	return 0;
 }
--- mat/1cpp/src/path.hpp
+++ mat/1cpp/src/path.hpp
@@ -1,62 +1,10 @@
 #ifndef PATH_HPP_EQS1DN5F
 #define PATH_HPP_EQS1DN5F
 
-#include <cstddef>
-#include <initializer_list>
-#include <queue>
-#include <stdexcept>
-#include <vector>
+#include "square_matrix.hpp"
 
-template<typename T>
-class AdjacencyMatrix {
-	T *matrix;
-	size_t m_size;
-
-public:
-	AdjacencyMatrix(size_t size)
-	{
-		this->m_size = size;
-		this->matrix = new T[size * size];
-	}
-
-	AdjacencyMatrix(std::initializer_list<std::initializer_list<T>> matrix)
-	{
-		this->m_size = matrix.size();
-		this->matrix = new T[m_size * m_size];
-
-		for (size_t i = 0; i < matrix.size(); ++i) {
-			if (matrix[i].size != this->m_size) {
-				delete[] this->matrix;
-				throw std::logic_error("A non-square matrix is provided");
-			}
-
-			for (size_t j = 0; j < matrix[i].size(); ++j) {
-				this->matrix[i * this->m_size + j] = std::move(matrix[i][j]);
-			}
-		}
-	}
-
-	~AdjacencyMatrix()
-	{
-		delete[] matrix;
-	}
-
-	T &operator[](size_t i, size_t j)
-	{
-		return matrix[i * m_size + j];
-	}
-
-	constexpr T &operator[](size_t i, size_t j) const
-	{
-		return matrix[i * m_size + j];
-	}
-
-	size_t size() const
-	{
-		return m_size;
-	}
-};
-
-std::vector<double> dijkstra(AdjacencyMatrix<double> adj_matr);
+std::vector<double> dijkstra(const SquareMatrix<double> &adj_matr, size_t start);
+std::pair<std::vector<double>, bool> ford_bellman(const SquareMatrix<double> &adj_matr, size_t start);
+SquareMatrix<double> floyd_warshall(const SquareMatrix<double> &adj_matr);
 
 #endif /* end of include guard: PATH_HPP_EQS1DN5F */
